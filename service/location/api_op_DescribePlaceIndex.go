ec4a2aff08e84d5cf570e654075121c061afc5cc
Jonatan Alexis Anauati <barakawins@gmail.com>
2021-11-12 09:22:47 -0300

    add missing import.

--- service/location/api_op_DescribePlaceIndex.go
+++ service/location/api_op_DescribePlaceIndex.go
@@ -5,6 +5,7 @@ package location
 import (
 	"context"
 	awsmiddleware "github.com/aws/aws-sdk-go-v2/aws/middleware"
+	"github.com/aws/aws-sdk-go-v2/aws/signer/v4"
 	"github.com/aws/aws-sdk-go-v2/service/location/types"
 	"github.com/aws/smithy-go/middleware"
 	smithyhttp "github.com/aws/smithy-go/transport/http"
